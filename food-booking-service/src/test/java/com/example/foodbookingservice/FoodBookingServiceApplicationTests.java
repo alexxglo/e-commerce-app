4991001b21c123278de85dfdf9cd488fa1d8826c
Alexandru Glodeanu <galex.phoenix@gmail.com>
2023-02-07 17:46:48 +0200

    changes

--- food-booking-service/src/test/java/com/example/foodbookingservice/FoodBookingServiceApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package com.example.foodbookingservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FoodBookingServiceApplicationTests {
-}
--- vendor/src/main/java/com/example/vendor/repository/VendorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.vendor.repository;
-
-import com.example.vendor.entity.Vendor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VendorRepository extends JpaRepository<Vendor, Long> {
-}
